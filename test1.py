6865bef91fda76f5b964b5606faa841173e526b2
dataninja99 <dataninja99@gmail.com>
2021-09-24 17:32:47 -0700

    Update test1.py

--- test1.py
+++ test1.py
@@ -3,7 +3,10 @@ import pytest
 def test1():
   assert 1==1
   print("xxxxxx")
+  
+def main():
+  test1()  
 
 if __name__ == "__main__":
     # execute only if run as a script
-    test1
+    test1()
